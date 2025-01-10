a867c5afb4134ed02631dec1d81bd2452272ba7f
Your Name <you@example.com>
2025-01-10 12:02:16 -0800

    idk

--- SnakeGPT.py
+++ SnakeGPT.py
@@ -1,10 +1,6 @@
 from rich import print
 import os
-head = "O"
-upsymb = "█"
-downsymb = "█"
-lefttsymb = "█"
-rightsymb = "█"
+import random
 
 board = [
     list("╭────────────────────╮"),
@@ -34,27 +30,62 @@ board = [
 
 global xhist
 global yhist
-xhist = []
-yhist = []
+global hx
+global hy
+global d
+global foodx
+global foody
+global length
+global eaten
+global runs
 
-# x coordinates begin at 1 and end at 20 (0 is a wall, 21 is a wall)
-hx = 10
-# y coordinates begin at 1 and end at 20 (0 is a wall, 21 is a wall)
-hy = 10
+def initsymbol():
+    global apple
+    global head
+    apple = "@"
+    head = "O"
+    upsymb = "█"
+    downsymb = "█"
+    lefttsymb = "█"
+    rightsymb = "█"
+initsymbol()
 
 def draw_board():
     for row in board:
         print("".join(row))
+    print(runs)
             
 def spawn_head():
     board[hy][hx] = f"{head}"
 
+def spawn_apple():
+    foodx = random.randint(1, 20)
+    foody = random.randint(1, 20)
+    board[foody][foodx] = f"{apple}"
+    eaten = False
+
+def detect_apple():
+    if hx == foodx and hy == foody:
+        length += 1
+        eaten = True
+
+def init():
+    # position history
+    xhist = []
+    yhist = []
+    # x coordinates begin at 1 and end at 20 (0 is a wall, 21 is a wall)
+    hx = 10
+    # y coordinates begin at 1 and end at 20 (0 is a wall, 21 is a wall)
+    hy = 10
+    # false means that the apple is still on the board that turn, true means that it has been eaten
+    eaten = False
+    # number of turns elapsed (for init)
+    runs = 0
+    spawn_apple()
+init()
+
 def cycle():
     dir = str(input("Which direction do you want to go? (WASD)\n"))
-    global hx
-    global hy
-    global d
-    global head
 
     try:
         if dir == "w":
@@ -93,12 +124,19 @@ def cycle():
         yhist.append(hy)
 
     spawn_head()
+    if eaten == True:
+        spawn_apple()
+    if runs != 0:
+        detect_apple()
+    runs += 1
     draw_board()
 
 if __name__ == "__main__":
     draw_board()
     spawn_head()
     draw_board()
+    print ('''\nWe reccomend the font "Kreative Square" for optimal display of the game.
+It can be obtained from https://www.kreativekorp.com/software/fonts/ksquare/\n''')
     while hx in range(1,21) and hy in range(1,21):
         cycle()
     print("You Lost :(")
